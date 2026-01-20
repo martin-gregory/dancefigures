c6be03bca9422e5f20690f3e947860057ad507fa
martin.gregory <martin.gregory@limeglass.com>
2026-01-20 12:24:27 +0100

    Remove dist directory from Git

--- dist/components/workshop-site.d.ts
+++
@@ -1,9 +0,0 @@
-import { LitElement } from 'lit';
-export declare class WorkshopSite extends LitElement {
-    private isMenuOpen;
-    static styles: import("lit").CSSResult;
-    connectedCallback(): void;
-    private toggleMenu;
-    private handleSubmit;
-    render(): import("lit-html").TemplateResult<1>;
-}
--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import './components/workshop-site';
-import './components/hero-parallax';
-import './components/instagram-gallery';
