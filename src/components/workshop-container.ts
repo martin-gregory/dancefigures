261ab5c36436b498840ccfe56b0e2efd5d00b182
martin.gregory <martin.gregory@limeglass.com>
2026-03-23 18:14:26 +0000

    Attempts to add mobile copy of all images

--- src/components/workshop-container.ts
+++ src/components/workshop-container.ts
@@ -23,9 +23,10 @@ export class WorkshopContainer extends LitElement {
   ];
   override render() {
     const isMobile = window.matchMedia('(max-width: 768px)').matches;
-    const imageSizes = {
+    const config = {
       heightVh: isMobile ? 80 : 220,
       panelHeight: isMobile ? 150 : 400,
+      imgUrl: isMobile ? '/img/mobile/' : '/img/',
     }
     return html`
       <workshop-site>
@@ -37,41 +38,41 @@ export class WorkshopContainer extends LitElement {
         <project-1 slot="project-1">
           <hero-parallax-panel
             slot="hero-1"
-            style="--panel-background-image: url('/img/held-background-coloured-big.avif'); --panel-height: ${imageSizes.panelHeight}vh;"
+            style="--panel-background-image: url('${config.imgUrl}held-background-coloured-big.avif'); --panel-height: ${config.panelHeight}vh;"
             .layers=${[
         {
           // last
-          src: '/img/held-wind-layer-3-big.avif',
+          src: `${config.imgUrl}held-wind-layer-3-big.avif`,
           speed: 0.08,
           topPct: 0,
-          heightVh: imageSizes.heightVh,
+          heightVh: config.heightVh,
           cssName: 'layer-3',
           fetchPriority: 'high',
         },
         {
           // middle
-          src: '/img/held-wind-layer-2a-swoosh-big.avif',
+          src: `${config.imgUrl}held-wind-layer-2a-swoosh-big.avif`,
           speed: 0.15,
           topPct: 10,
-          heightVh: imageSizes.heightVh,
+          heightVh: config.heightVh,
           cssName: 'layer-2',
           fetchPriority: 'high',
         },
         {
           // middle
-          src: '/img/held-wind-layer-2b-swoosh-big.avif',
+          src: `${config.imgUrl}held-wind-layer-2b-swoosh-big.avif`,
           speed: 0.1,
           topPct: 10,
-          heightVh: imageSizes.heightVh,
+          heightVh: config.heightVh,
           cssName: 'layer-2',
           fetchPriority: 'high',
         },
         {
           // front
-          src: '/img/held-wind-layer-1a-swoosh-big.avif',
+          src: `${config.imgUrl}held-wind-layer-1a-swoosh-big.avif`,
           speed: 0.1,
           topPct: 50,
-          heightVh: imageSizes.heightVh,
+          heightVh: config.heightVh,
           scale: 0.8,
           leftPct: 9,
           cssName: 'layer-1',
@@ -79,10 +80,10 @@ export class WorkshopContainer extends LitElement {
         },
         {
           // front
-          src: '/img/held-wind-layer-1b-swoosh-big.avif',
+          src: `${config.imgUrl}held-wind-layer-1b-swoosh-big.avif`,
           speed: 0.04,
           topPct: 50,
-          heightVh: imageSizes.heightVh,
+          heightVh: config.heightVh,
           scale: 0.8,
           leftPct: 9,
           cssName: 'layer-1',
@@ -95,8 +96,8 @@ export class WorkshopContainer extends LitElement {
 
           <shrink-painting-panel
             slot="shrink-layers-parallax"
-            stageImage="/img/interior-held-wind.avif"
-            .layers=${[{ src: '/img/held-wind-painting.avif', speed: 0, objectFit: 'contain', startPos: '-320', cssName: 'background' }]}
+            stageImage="${config.imgUrl}interior-held-wind.avif"
+            .layers=${[{ src: `${config.imgUrl}held-wind-painting.avif`, speed: 0, objectFit: 'contain', startPos: '-320', cssName: 'background' }]}
           >
           </shrink-painting-panel>
         </project-1>
@@ -105,31 +106,31 @@ export class WorkshopContainer extends LitElement {
         <project-2 slot="project-2">
           <hero-parallax-panel
             slot="hero-2"
-            style="--panel-background-image: url('/img/moved-by-tide-bg.avif'); --panel-height: ${imageSizes.panelHeight}vh;"
+            style="--panel-background-image: url('${config.imgUrl}moved-by-tide-bg.avif'); --panel-height: ${config.panelHeight}vh;"
             .words=${['Moved', 'by', 'the', 'Tide']}
             .layers=${[
         {
           speed: 0.1,
-          src: '/img/moved-by-tide-layer-3-sm.avif',
+          src: `${config.imgUrl}moved-by-tide-layer-3-sm.avif`,
           leftPct: 20,
           topPct: 0,
-          heightVh: imageSizes.heightVh,
+          heightVh: config.heightVh,
           scale: 0.8,
           cssName: 'layer-3',
         },
         {
-          src: '/img/moved-by-tide-layer-2-sm.avif',
+          src: `${config.imgUrl}moved-by-tide-layer-2-sm.avif`,
           speed: 0.17,
           topPct: 10,
-          heightVh: imageSizes.heightVh,
+          heightVh: config.heightVh,
           leftPct: 0,
           cssName: 'layer-2',
         },
         {
           speed: 0.01,
-          src: '/img/moved-by-tide-layer-1-sm.avif',
+          src: `${config.imgUrl}moved-by-tide-layer-1-sm.avif`,
           topPct: 30,
-          heightVh: imageSizes.heightVh,
+          heightVh: config.heightVh,
           cssName: 'layer-1',
         },
       ]}
@@ -137,9 +138,9 @@ export class WorkshopContainer extends LitElement {
           </hero-parallax-panel>
           <shrink-painting-panel
             slot="shrink-layers-parallax"
-            stageImage="/img/interior-moved-by-tide.avif"
+            stageImage="${config.imgUrl}interior-moved-by-tide.avif"
             .layers=${[
-        { src: '/img/moved-by-tide-painting.avif', speed: 0, objectFit: 'contain', startPos: '-320', cssName: 'background' },
+        { src: `${config.imgUrl}moved-by-tide-painting.avif`, speed: 0, objectFit: 'contain', startPos: '-320', cssName: 'background' },
       ]}
           >
           </shrink-painting-panel>
@@ -147,32 +148,32 @@ export class WorkshopContainer extends LitElement {
         <project-2 slot="project-3">
           <hero-parallax-panel
             slot="hero-2"
-            style="--panel-background-image: url('/img/dunes-1-background.avif'); --panel-height: ${imageSizes.panelHeight}vh;"
+            style="--panel-background-image: url('${config.imgUrl}dunes-1-background.avif'); --panel-height: ${config.panelHeight}vh;"
             .words=${['Dunes', 'I']}
             .layers=${[
         {
           speed: 0.14,
-          src: '/img/dunes-1-layer-3a.avif',
+          src: `${config.imgUrl}dunes-1-layer-3a.avif`,
           topPct: 15,
-          heightVh: imageSizes.heightVh,
+          heightVh: config.heightVh,
           scale: 0.8,
           cssName: 'layer-3',
         },
         {
-          src: '/img/dunes-1-layer-2a.avif',
+          src: `${config.imgUrl}dunes-1-layer-2a.avif`,
           speed: 0.17,
           topPct: 0,
-          heightVh: imageSizes.heightVh,
+          heightVh: config.heightVh,
           scale: 0.8,
           leftPct: 20,
           cssName: 'layer-2',
         },
         {
           speed: 0.01,
-          src: '/img/dunes-1-layer-1a.avif',
+          src: `${config.imgUrl}dunes-1-layer-1a.avif`,
           topPct: 50,
           leftPct: 24,
-          heightVh: imageSizes.heightVh,
+          heightVh: config.heightVh,
           scale: 0.5,
           cssName: 'layer-1',
         },
@@ -181,51 +182,51 @@ export class WorkshopContainer extends LitElement {
           </hero-parallax-panel>
           <shrink-painting-panel
             slot="shrink-layers-parallax"
-            stageImage="/img/interior-dunes-2.avif"
+            stageImage="${config.imgUrl}interior-dunes-2.avif"
             stageImageEndTranslateYPos="15"
-            .layers=${[{ src: '/img/dunes-1-painting.avif', speed: 0, objectFit: 'contain', startPos: '-320', cssName: 'background' }]}
+            .layers=${[{ src: `${config.imgUrl}dunes-1-painting.avif`, speed: 0, objectFit: 'contain', startPos: '-320', cssName: 'background' }]}
           >
           </shrink-painting-panel>
         </project-2>
         <project-2 slot="project-4">
           <hero-parallax-panel
             slot="hero-2"
-            style="--panel-background-image: url('/img/dunes-2-background.avif'); --panel-height: ${imageSizes.panelHeight}vh;"
+            style="--panel-background-image: url('${config.imgUrl}dunes-2-background.avif'); --panel-height: ${config.panelHeight}vh;"
             .words=${['Dunes', 'II']}
             .layers=${[
 
         {
           speed: 0.14,
-          src: '/img/dunes-2-layer-4a.avif',
+          src: `${config.imgUrl}dunes-2-layer-4a.avif`,
           topPct: 25,
-          heightVh: imageSizes.heightVh,
+          heightVh: config.heightVh,
           scale: 0.85,
           leftPct: 6,
           cssName: 'layer-4',
         },
         {
           speed: 0.14,
-          src: '/img/dunes-2-layer-3a.avif',
+          src: `${config.imgUrl}dunes-2-layer-3a.avif`,
           topPct: 25,
-          heightVh: imageSizes.heightVh,
+          heightVh: config.heightVh,
           scale: 0.85,
           cssName: 'layer-3',
         },
         {
-          src: '/img/dunes-2-layer-2a.avif',
+          src: `${config.imgUrl}dunes-2-layer-2a.avif`,
           speed: 0.17,
           topPct: 0,
-          heightVh: imageSizes.heightVh,
+          heightVh: config.heightVh,
           scale: 0.8,
           leftPct: 15,
           cssName: 'layer-2',
         },
         {
           speed: 0.01,
-          src: '/img/dunes-2-layer-1a.avif',
+          src: `${config.imgUrl}dunes-2-layer-1a.avif`,
           topPct: 50,
           leftPct: 24,
-          heightVh: imageSizes.heightVh,
+          heightVh: config.heightVh,
           // scale: 0.8,
           cssName: 'layer-1',
         },
@@ -234,9 +235,9 @@ export class WorkshopContainer extends LitElement {
           </hero-parallax-panel>
           <shrink-painting-panel
             slot="shrink-layers-parallax"
-            stageImage="/img/interior-dunes-2.avif"
+            stageImage="${config.imgUrl}interior-dunes-2.avif"
             stageImageEndTranslateYPos="15"
-            .layers=${[{ src: '/img/dunes-2-painting.avif', speed: 0, objectFit: 'contain', startPos: '-320', cssName: 'background' }]}
+            .layers=${[{ src: `${config.imgUrl}dunes-2-painting.avif`, speed: 0, objectFit: 'contain', startPos: '-320', cssName: 'background' }]}
           >
           </shrink-painting-panel>
         </project-2>
