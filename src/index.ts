331b2302f797c120c7fa2d2afc048ec4fef69f8a
martin.gregory <martin.gregory@limeglass.com>
2026-04-03 10:44:06 +0100

    Adds basic analytics @microsoft/clarity

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import "./components/site-navigation";
-import "./components/workshop-container";
+import './components/site-navigation';
+import './components/workshop-container';
 import './components/workshop-site';
 import './components/hero-parallax';
 import './components/hero-parallax-panel';
@@ -9,9 +9,20 @@ import './components/hero-scroll';
 import './components/image-parallax-container';
 import './components/pattern-parallax-container';
 import './components/progress-parallax-container';
-import './components/shrink-painting-panel'
-import './components/shrink-layers-parallax'
+import './components/shrink-painting-panel';
+import './components/shrink-layers-parallax';
 import './components/project-1';
 import './components/project-2';
 
 // Can add app bootstrapping logic here if needed
+import Clarity from '@microsoft/clarity';
+// Make sure to add your actual project id instead of "yourProjectId".
+
+// Only fire in production to save your quota and keep data clean
+const isProd = import.meta.env ? import.meta.env.PROD : process.env.NODE_ENV === 'production';
+
+if (isProd) {
+  const projectId = 'w5flc55tv4';
+  Clarity.init(projectId);
+  console.log('Clarity initialized for Production');
+}
